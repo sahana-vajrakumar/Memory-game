51ffc5a9b9212e0e79d2c96009157b9526bf4732
Sandeep R <Sandy@192-168-1-6.tpgi.com.au>
2017-12-03 22:31:05 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardTwo);
+
+var cards = ["queen" , "queen" , "king" , "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay[0] === cardsInPlay[1])
+{
+	alert("you found a match");
+} else {
+	alert("sorry try again");
+}
+
+
+
